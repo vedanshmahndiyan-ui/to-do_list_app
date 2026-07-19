63edde099f234611190d9ac93f03e0d99d7b0930
Vedansh Singh <vedanshmahndiyan@gmail.com>
2026-07-19 16:24:48 +0530

    completed goal 3 for week 3

--- to-do_app.py
+++ to-do_app.py
@@ -10,3 +10,24 @@ for i in range(num_tasks):
 print('Here is your to-do list for today: ')
 for index, item in enumerate(tasks, start=1):
     print(f"{index}. {item}")
+
+while True:
+    print('\nOptions -> view tasks[v], add task[a], quit[q]')
+    choice = input('Enter your choice: ').lower()
+    if choice == 'v':
+        print('Here is your to-do list for today: ')
+        for index, item in enumerate(tasks, start=1):
+            print(f"{index}. {item}")
+    elif choice == 'a':
+        new_task = input("Enter the new task: ")
+        tasks.append(new_task)
+        print(f"Task '{new_task}' added to your to-do list.")
+    elif choice == 'q':
+        print('Exiting the To-Do List App. Goodbye!')
+        break
+    elif choice == 'ilovedoom':
+        print('You have selected the secret option!')
+        print('Fun Fact[totally not tampered with]: Doom is the best game ever made, if you like anything else...just know that I will find you and make you play Doom until you like it. You have been warned. ')
+    else:
+        print('Invalid choice. Please enter a valid option.')
+        
