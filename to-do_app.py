729aaa669549150b76a108b4a8847f926b454212
Vedansh Singh <vedanshmahndiyan@gmail.com>
2026-07-19 15:56:56 +0530

    nfurnished the code
    Removed commented-out code for task input and added dynamic task input based on user-defined number of tasks.

--- to-do_app.py
+++ to-do_app.py
@@ -1,13 +1,6 @@
 user_name = input("Enter your name: ")
 print(f"Hello, {user_name}! Welcome to the To-Do List App.")
 
-# task1 = []
-# task1.append(input("Enter your task: "))
-# print(f"Task 1: {task1[0]} has been added to your to-do list.")
-# task1.append(input("Enter your task: "))
-# print(f"Task 2: {task1[1]} has been added to your to-do list.")
-# task1.append(input("Enter your task: "))
-# print(f"Task 3: {task1[2]} has been added to your to-do list.")
 tasks = []
 num_tasks = int(input('how many tasks do you want to add to your to-do list?: '))
 for i in range(num_tasks):
